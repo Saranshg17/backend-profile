56be1d0b434bde8004714ca4c31586ba4f159afd
Saransh Goyal <saranshgoyal@Saranshs-MacBook-Air.local>
2024-02-09 23:57:34 +0530

    middlewares and custom api responses and error handlings

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,6 @@ import {DB_NAME} from "../constants.js";
 
 const connectDB=async()=>{
     try{
-        console.log("mongoDB url:",`${process.env.MONGODB_URI}/${DB_NAME}`)
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`\n MongoDB connected !! DB host: ${connectionInstance.connection.host}`);
     }catch(error){
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from 'dotenv';
+import { app } from './app.js';
+import connectDB from "./db/index.js";
 
 dotenv.config({
     path:"./.env"
 });
 
-
-import connectDB from "./db/index.js";
-
-// dotenv.config({
-//     path: "./env"
-// })
-
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("Error encounted connecting to server:",error)
+        process.exit(1);
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed:",err);
+})
