42b49f81730715bf716059c9f0bf17cb4c3ac9c8
Saransh Goyal <saranshgoyal@Saranshs-MacBook-Air.local>
2024-02-11 21:57:34 +0530

    Add Registeration controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,73 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { users } from "../models/user.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req,res) => {
-    res.status(200).json({
-        message:"ok"
+    //get user details from frontend
+    const {email,password,profile, Default,categories}=req.body
+    console.log("email:",email);
+    console.log("categories:",categories)
+
+    //validation - not empty
+    if(Default===None){
+        Default=false
+    }
+    if(categories===None){
+        throw new ApiError(400, "Please select atleast one category")
+    }
+    if(categories===[]){
+        throw new ApiError(400, "Please select atleast one category")
+    }
+    if(
+        [email,password,profile].some((field)=>field?.trim()==="")
+    ){
+        throw new ApiError(400, "Some required fields are empty")
+    }
+
+
+    // check if already exists: email, profile
+    const existedprofile = users.findOne({
+        $and: [{ email },{ profile }]
     })
+    if (existedprofile){
+        throw new ApiError(409,"Profile already exists")
+    }
+
+    //check whether password is right or not if user already exists
+
+
+    //if user didn't exist already change default to true
+    const existeduser = users.findOne(email)
+
+    if (!existeduser){
+        Default=true
+    }
+
+    //create user object-create entry in db
+    const user = await users.create({
+        email,
+        password,
+        profile,
+        Default:Default || false,
+        categories
+    })
+
+    //remove password and refresh token field from response
+    const createdProfile = await users.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    //check for user creation
+    if(!createdProfile){
+        throw new ApiError(500,"Something went wrong while registering the user")
+    }
+
+
+    //return response or send error
+    return res.status(201).json(
+        new ApiResponse(200,createdProfile,"profile registered successfully")
+    )
 })
 
 export {registerUser}
